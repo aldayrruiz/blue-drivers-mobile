3f5adedd43be8098c89bc0c96523eed00de0fcaf
Aldayr Ruiz <aldayr.ruiz@opendeusto.es>
2022-04-01 13:32:05 +0200

    chore: App name to BLUE Drivers

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'eu.bluece.fleetmanagement',
-  appName: 'BlueCe Fleet Management',
+  appId: 'eu.bluece.drivers',
+  appName: 'BLUE Drivers',
   webDir: 'www',
   bundledWebRuntime: false,
 };

9dc115716c81d847402ffca9e5f072cf02cdbe7c
Aldayr Ruiz <aldayr.ruiz@opendeusto.es>
2022-04-07 14:07:02 +0200

    refactor: Refactor

--- src/app/core/pipes/custom-pipes.module.ts
+++ src/app/core/pipes/custom-pipes.module.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { DistanceFromNow } from './distance-from-now.pipe';
 import { DurationPipe } from './duration.pipe';
-import { MediumDatePipe } from './essential-date.pipe';
 import { NotTooLongPipe } from './not-too-long.pipe';
 import { FromKnotsToKphPipe } from './to-kph.pipe';
 
 @NgModule({
-  declarations: [DurationPipe, MediumDatePipe, NotTooLongPipe, FromKnotsToKphPipe],
+  declarations: [DurationPipe, DistanceFromNow, NotTooLongPipe, FromKnotsToKphPipe],
   imports: [CommonModule],
-  exports: [DurationPipe, MediumDatePipe, NotTooLongPipe, FromKnotsToKphPipe],
+  exports: [DurationPipe, DistanceFromNow, NotTooLongPipe, FromKnotsToKphPipe],
 })
 export class CustomPipesModule {}
--- src/app/core/pipes/distance-from-now.pipe.ts
+++ src/app/core/pipes/distance-from-now.pipe.ts
@@ -1,13 +1,11 @@
-import { DatePipe } from '@angular/common';
 import { Pipe, PipeTransform } from '@angular/core';
 import { formatDistanceToNowStrict } from 'date-fns';
 import es from 'date-fns/locale/es';
 
-@Pipe({ name: 'essential' })
-export class MediumDatePipe implements PipeTransform {
+@Pipe({ name: 'distance' })
+export class DistanceFromNow implements PipeTransform {
   transform(date: Date | string, day: number, format: string = 'MMMM d, y, HH:mm'): string {
     const d = new Date(date);
     return formatDistanceToNowStrict(d, { addSuffix: true, locale: es });
-    return new DatePipe('es-ES').transform(d, format);
   }
 }

dbef9b792aaffef1508df6b27baf5599661f7e95
Aldayr Ruiz <aldayr.ruiz@opendeusto.es>
2022-04-28 09:30:07 +0200

    chore: Some ios stuff

---
+++ src/app/core/services/view/vehicle-icon.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class VehicleIconProvider {
+  private basePath = 'icon/vehicles/';
+  private icons = [
+    'yellow-vehicle.png',
+    'red-vehicle.png',
+    'purple-vehicle.png',
+    'orange-vehicle.png',
+    'grey-vehicle.png',
+    'green-vehicle.png',
+    'blue-vehicle.png',
+  ];
+
+  /**
+   *
+   * @returns an array of vehicle icon paths
+   */
+  getIconsPaths() {
+    const icons = this.icons.map((file) => this.basePath + file);
+    return icons;
+  }
+
+  getIconPathForVehicleIcon(vehicleIcon: number) {
+    return this.getIconsPaths()[vehicleIcon];
+  }
+}

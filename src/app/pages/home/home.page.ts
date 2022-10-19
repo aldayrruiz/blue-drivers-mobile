9c5dff5e43fec9d4fc83ed8599995d7d190eb3ae
Aldayr Ruiz <aldayr.ruiz@opendeusto.es>
2022-10-19 13:27:37 +0200

    NEw version available

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AppUpdate, AppUpdateInfo } from '@capawesome/capacitor-app-update';
-import { AlertController, Platform } from '@ionic/angular';
 import { homeButtons } from './home-buttons';
 
 @Component({
@@ -12,38 +10,5 @@ export class HomePage implements OnInit {
   toolbarTitle = 'Home';
   buttons = homeButtons;
 
-  constructor(private platform: Platform, private alertController: AlertController) {}
-
-  ngOnInit() {
-    if (!this.platform.is('capacitor')) {
-      return;
-    }
-    AppUpdate.getAppUpdateInfo().then((info: AppUpdateInfo) => {
-      const { availableVersion } = info;
-      if (Number('2.2') < Number(availableVersion)) {
-        this.showNewVersionAvailable();
-      }
-    });
-  }
-
-  private async showNewVersionAvailable() {
-    const button = {
-      text: 'Actualizar',
-      role: 'confirm',
-      handler: async () => {
-        await this.openAppStore();
-      },
-    };
-
-    const alert = await this.alertController.create({
-      header: 'Nueva version disponible',
-      message: 'Pulsa en actualizar para descargar la nueva version',
-      buttons: [button],
-    });
-    await alert.present();
-  }
-
-  private async openAppStore(): Promise<void> {
-    await AppUpdate.openAppStore();
-  }
+  ngOnInit() {}
 }
